f096dde496bd90c6ca632b15f63f01ef9036dfc6
datorogavaa <datorogavaa@gmail.com>
2025-03-19 18:37:24 +0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,81 @@
 import java.util.Scanner;
 
 public class Main {
+//    int x = 0;
+//    static double balance = 10000;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String[] x =  {"dato", "rogava" };
+        System.out.println(x);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter your age: ");
+//        Integer Age = scanner.nextInt();
+//
+//        System.out.println("Enter your name: ");
+//        String name = scanner.next();
+//
+//        System.out.println("You are " + Age + "years old");
+//        System.out.println("You are name is " + name );
 
-        System.out.print("Enter your age: ");
-        Integer Age = scanner.nextInt();
+//
+//        scanner.close();
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Whats your name? ");
+//        String name = scanner.next();
+//
+//        happyBirthday(name);
+//
+//    }
+//    static void happyBirthday(String name) {
+//        System.out.printf("Happy Birthday to %s", name);
+//        System.out.println("\nHappy Birthday dear you");
+//        System.out.println("How old did you become?");
+//    }
 
-        System.out.println("Enter your name: ");
-        String name = scanner.next();
-
-        System.out.println("You are " + Age + "years old");
-        System.out.println("You are name is " + name );
-
-
-        scanner.close();
-
-    }}
+//    bank();
+//
+//    }
+//    static Integer bankBalance(Integer balance) {
+//            return balance;
+//    }
+//    static double bankBalance() {
+//        return balance;
+//    }
+//
+//    static String withdraw(double money) {
+//        return "You did Withdraw with " + money + "GEL";
+//    }
+//    static void  bank() {
+//
+//        boolean working = true;
+//        Scanner scanner = new Scanner(System.in);
+//
+//        do {
+//            System.out.println("\n*************************");
+//            System.out.println("------1. $show Balance$-----");
+//            System.out.println("------2. Deposit >-----");
+//            System.out.println("------3. Withdraw <------");
+//            System.out.println("------4. Exit------");
+//            System.out.println("*************************");
+//            System.out.print("Choose number 1 to 4: ");
+//            Integer choice = scanner.nextInt();
+//            switch (choice) {
+//                case 1 -> System.out.printf("Balance is %.2f", bankBalance());
+//                case 2 -> System.out.println("Deposit");
+//                case 3 -> {
+//                    System.out.println("How much GELA do you want? ");
+//                    Double money = scanner.nextDouble();
+//                    System.out.println(withdraw(money));
+//                    balance = balance - money;
+//                }
+//                case 4 -> {
+//                    working = false;
+//                    System.out.println("Exit");
+//                }
+//                default -> System.out.println("Wrong Choice");
+//            }
+//        }while ( working );
+//        scanner.close();
+    }
+}
